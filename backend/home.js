0994cd865c1765a3a98e2e19e197894ad05c3705
kavi <nithveenkavi@yahoo.com>
2025-06-14 22:05:18 +0530

    completed till home aboutus franchise

---
+++ backend/home.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector(".slidebar")
+    sidebar.style.display = 'flex'
+}
+function hidesidebar(){
+    const sidebar = document.querySelector(".slidebar")
+    sidebar.style.display = 'none'
+}
